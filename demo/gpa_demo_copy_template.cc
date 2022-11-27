6c3e18f4b57f87f49f4709068c610f33a47d0227
Ruben <83189955+0x17io@users.noreply.github.com>
2022-11-26 18:37:40 -0600

    Update gpa_demo_copy_template.cc

--- demo/gpa_demo_copy_template.cc
+++ demo/gpa_demo_copy_template.cc
@@ -1,17 +1,40 @@
-short Calculator::process(short x, short y, char op) {
-  if (op == '+') {
-    return x + y;
-  } else if (op == '-') {
-    return x - y;
-  } else if (op == '*') {
-    return x * y;
-  } else {
-    return -1;
-  }
+short Calculator::process(char gpa_data[]) {
+    float sum = 0;
+    float number_of_grades = 0;
+
+    // Go through string, only add numbers
+    for (int i = 0; gpa_data[i] != '\0'; i++) {
+        if (gpa_data[i] == ' ') {
+            continue;
+        }
+        else {
+            number_of_grades += 1;
+            switch (gpa_data[i]) {
+            case 'A':
+                sum += 4;
+                break;
+            case 'B':
+                sum += 3;
+                break;
+            case 'C':
+                sum += 2;
+                break;
+            case 'D':
+                sum += 1;
+                break;
+            case 'F':
+                sum += 0;
+                break;
+            }
+        }
+    } 
+
+
+    return sum / (number_of_grades * 4);
 }
 
 // TODO: Way to mark Calculator::process() as main function
 #pragma hls_top
-short my_package(Calculator &calc, short x, short y, char op) {
-  return calc.process(x, y, op);
+short my_package(Calculator &calc, char gpa_data[]) {
+  return calc.process(gpa_data);
 }
